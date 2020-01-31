6537f2d2d54b9c29362e7dbbf39211611071414c
Alejo117lopez <60120226+Alejo117lopez@users.noreply.github.com>
2020-01-30 20:33:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cálculo_Cuota_moderadora
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese el valor de su salario (En números): ");
+            double s = double.Parse(Console.ReadLine());
+
+            double sm = 877803.00;
+
+            if (s < (2 * sm))
+            {
+                Console.WriteLine("Usted es Tarifa A, y el valor de su cuota moderadora es de $3.400");
+            }
+
+            else if ((2*sm)<= s && s < (5 * sm))
+            {
+                Console.WriteLine("Usted es Tarifa B, y el valor de su cuota moderadora es de $13.500");
+            }
+            else
+            {
+                Console.WriteLine("Usted es Tarifa C, y el valor de su cuota moderadora es de $35.600");
+            }
+        }
+    }
+}
