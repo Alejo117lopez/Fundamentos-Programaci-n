57a36080d110aa6525ae07d9ed10e90ee6603865
Alejo117lopez <60120226+Alejo117lopez@users.noreply.github.com>
2020-01-30 20:33:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cálculo_de_tarifa
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese en números la cantidad de su salario mensual: ");
+            double s = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese en números la cantidad de UN (1) SMMLV: ");
+            double sm = double.Parse(Console.ReadLine());
+
+            if (s < (2 * sm)) { 
+            
+               Console.WriteLine("Usted es Tarifa A");
+            }
+
+            else if ( (2*sm)<= s && s< (4 * sm))
+            {
+                Console.WriteLine("Usted es Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Usted es Tarifa C");
+            }
+
+        }
+    }
+}
